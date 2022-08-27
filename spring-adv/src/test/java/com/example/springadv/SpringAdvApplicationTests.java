7fed91d9f0840789e7017b318db662223e53ec0a
Sp4ngl3r <hemanth921941@gmail.com>
2022-08-27 12:59:32 +0530

    Added another project

---
+++ spring-adv/src/test/java/com/example/springadv/SpringAdvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springadv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
